a42718ec1d5fed674dbb011c4459fdeefd15c623
Ogannes <toxicyouth@yandex.ru>
2021-07-26 23:47:31 +0300

    исправить скрипты

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,33 @@
 let wrapper = document.querySelector('.wrapper');
 let profileEditButton = wrapper.querySelector('.profile__edit-button');
 let popupCloseButton = wrapper.querySelector('.popup__close-button');
-let submitButton = wrapper.querySelector('.popup__submit-button');
 let popup = wrapper.querySelector('.popup');
-
-//открытие и закрытие попапа
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
-}
-
-profileEditButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-submitButton.addEventListener('click', popupToggle);
-
-//изменение данных при отправке формы
+let username = wrapper.querySelector('.profile__user-name');
+let userJob = wrapper.querySelector('.profile__user-job');
 let form = document.querySelector('.popup__form');
 let nameInput = form.querySelector('.popup__info_type_username');
 let jobInput = form.querySelector('.popup__info_type_job');
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  let username = wrapper.querySelector('.popup__info_username');
-  let userJob = wrapper.querySelector('.profile__user-job');
-  username.textContent = nameInput.value;
-  userJob.textContent = jobInput.value;
+//открытие попапа со вставкой данных из profile в форму
+function popupOpen() {
+  nameInput.value = username.textContent;
+  jobInput.value = userJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
+//закрытие попапа
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+//изменение данных при отправке формы
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  username.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  popupClose();
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 form.addEventListener('submit', formSubmitHandler);
